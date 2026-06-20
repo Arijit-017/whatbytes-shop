d16c466e960c17fe0b90c123886eb2b896f99470
Arijit Ghosh <ajghosh017@gmail.com>
2026-06-20 09:35:53 +0530

    Update product page

---
+++ app/product/[id]/page.js
@@ -0,0 +1,19 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function ProductDetail() {
+    return (
+        <div>
+            <Header/>
+            <main>
+                <div>Back to Product</div>
+                <div>
+                    <div>Product Image</div>
+                    <div>Product Details</div>
+                </div>
+                <div>Related products</div>
+            </main>
+            <Footer/>
+        </div>
+    )
+}
